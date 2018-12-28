145fae124dc9127c3a97f7550a2ab430b19d6067
Osman Omar <osmannamso123@gmail.com>
2018-12-28 07:25:24 +0000

    asd

--- UnieBlog/urls.py
+++ UnieBlog/urls.py
@@ -20,6 +20,5 @@ from django.views.decorators.csrf import csrf_exempt
 from blog.views import tol
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('toltru', tol)
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
