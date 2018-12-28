e0e3a5112d2bece71855b52460f2b37eaa619865
osmannamso123@gmail.com <osmannamso123@gmail.com>
2018-12-28 13:20:12 +0600

    asdsad

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ def tol(request):
     imgs = ['https://media.self.com/photos/58adf9d18557fe118ba6098a/4:3/w_728,c_limit/gigi-hadid-reebok.png', 'https://www.esquire.my/uploadfiles/images/contents/women/horizontal/emrata_photoshop.jpg', 'http://celebrityinsider.org/wp-content/uploads/2018/09/Leonardo-DiCaprio-WallPapersr.FilmiBeat.com_-e1538073010143.jpg', 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/01/09/amber-heard_0.jpg?w968h681']
     allTopics = Topic.objects.all()
     q = ''
+    Article.objects.all().delete()
     for topic in allTopics:
         a = random.randint(0, 3)
         article1 = Article(title='Диета после праздников', description='Диета это пресс', content='This is content of article', author_id=15, img_url=imgs[a], helpful=True, seen=random.randint(1,100), topic_id=topic.id)
@@ -19,10 +20,10 @@ def tol(request):
                            content='This is content of article', author_id=15, img_url=imgs[a], helpful=True,
                            seen=random.randint(1, 100), topic_id=topic.id)
         article5 = Article(title='Диета после праздников', description='Диета это пресс',
-                           content='This is content of article', author_id=15, img_url=imgs[a], helpful=True,
+                           content='This is content of article', author_id=15, img_url=imgs[a], helpful=False,
                            seen=random.randint(1, 100), topic_id=topic.id)
         article6 = Article(title='Диета после праздников', description='Диета это пресс',
-                           content='This is content of article', author_id=15, img_url=imgs[a], helpful=True,
+                           content='This is content of article', author_id=15, img_url=imgs[a], helpful=False,
                            seen=random.randint(1, 100), topic_id=topic.id)
         article1.save()
         article2.save()
